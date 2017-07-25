4a83dcc3de048d6fe59b9298ce0ea40d1ef71316
sumitsaiwal <sumitsaiwal@gmail.com>
2017-07-25 19:11:43 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,44 +1,47 @@
-	package com.easyleave;
+package com.leave;
 	import java.sql.Connection;
 	import java.sql.DriverManager;
 	import java.sql.SQLException;
-	import java.net.*;
 	import java.util.Properties;
 	 
 	public class DBConnection {
-	 	String host = System.getenv("db_host");
-		String database = System.getenv("db_name");
-		String user = System.getenv("db_user");
-		String password = System.getenv("db_pass");
-		
+		static String host = System.getenv("db_host");
+		static String database = System.getenv("db_name");
+		static String user = System.getenv("db_user");
+		static String password = System.getenv("db_pass");
+	 
 	    public static Connection getConnection() {
-			Connection con = null;
-			try {
-			
-			String url = String.format("jdbc:mysql://%s/%s", host, database);
+	        Connection con = null;
+	        
+	            try {
+	            	String url = String.format("jdbc:mysql://%s/%s", host, database);
 
-			// Set connection properties.
-			Properties properties = new Properties();
-			properties.setProperty("user", user);
-			properties.setProperty("password", password);
-			properties.setProperty("useSSL", "true");
-			properties.setProperty("verifyServerCertificate", "true");
-			properties.setProperty("requireSSL", "false");
-				
-			Class.forName("com.mysql.jdbc.Driver");
-		    	con = DriverManager.getConnection(System.getenv(url, properties));	
-			}
-		    
-			catch (SQLException e) {
+	    			// Set connection properties.
+	    			Properties properties = new Properties();
+	    			properties.setProperty("user", user);
+	    			properties.setProperty("password", password);
+	    			properties.setProperty("useSSL", "true");
+	    			properties.setProperty("verifyServerCertificate", "true");
+	    			properties.setProperty("requireSSL", "false");
+	    			
+	    			Class.forName("com.mysql.jdbc.Driver");
+		 
+		            // create the connection now
+	    			con = DriverManager.getConnection(url, properties);	
+				} 
+	            
+	            catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} 
-				
-			catch (ClassNotFoundException e) {
+				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} 
-		return con;
-		}
+				}
+	            
+	 
+	        return con;
+	    }
 	    
 	}
+
+
